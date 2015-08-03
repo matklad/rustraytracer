e62684ad04dfc763b41c60e3ce2b0405c1033a99
Aleksey Kladov <aleksey.kladov@gmail.com>
2015-08-04 01:30:24 +0300

    move obj parser to separate file

--- src/geom/shape/mesh/mod.rs
+++ src/geom/shape/mesh/mod.rs
@@ -1,4 +1,5 @@
 mod bvh;
+mod obj_parser;
 
 use std::fmt;
 use std::error::Error;
@@ -9,24 +10,10 @@ use geom::ray::Ray;
 use super::bound_box::{BoundBox, Bound};
 use super::{Triangle, Shape, Intersection};
 use self::bvh::Bvh;
+use self::obj_parser::ObjParser;
 
 pub struct Mesh {
     index: Bvh<Triangle>
-}
-
-#[derive(Debug)]
-pub struct ParseObjError;
-
-impl Error for ParseObjError {
-    fn description(&self) -> &str {
-        "Invalid .obj file"
-    }
-}
-
-impl fmt::Display for ParseObjError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        self.description().fmt(f)
-    }
 }
 
 impl Mesh {
@@ -37,13 +24,7 @@ impl Mesh {
     }
 
     pub fn from_obj(source: &mut io::Read) -> Result<Mesh, Box<Error>> {
-        let parser = ObjParser {
-            points: Vec::new(),
-            normals: Vec::new(),
-            faces: Vec::new(),
-        };
-
-        parser.parse(source)
+        ObjParser::new().parse(source)
     }
 }
 
@@ -51,81 +32,5 @@ impl Mesh {
 impl Shape for Mesh {
     fn intersect(&self, ray: &Ray) -> Option<Intersection> {
         self.index.intersect(ray)
-    }
-}
-
-
-struct ObjParser {
-    points: Vec<Point>,
-    normals: Vec<UnitVector>,
-    faces: Vec<Triangle>,
-}
-
-impl ObjParser {
-    fn parse(mut self, source: &mut io::Read) -> Result<Mesh, Box<Error>> {
-        let mut s = String::new();
-        try!(source.read_to_string(&mut s));
-        for line in s.lines() {
-            if line.starts_with("v ") {
-                try!(self.parse_vertex(line));
-            } else if line.starts_with("vn ") {
-                try!(self.parse_normal(line));
-            } else if line.starts_with("f ") {
-                try!(self.parse_face(line));
-            }
-        }
-
-        Ok(Mesh::new(self.faces))
-    }
-
-    fn parse_vertex(&mut self, s: &str) -> Result<(), Box<Error>> {
-        let coords = try!(ObjParser::parse_coordinates(s));
-        self.points.push(Point::new(coords.0, coords.1, coords.2));
-        Ok(())
-    }
-
-    fn parse_normal(&mut self, s: &str) -> Result<(), Box<Error>> {
-        let coords = try!(ObjParser::parse_coordinates(s));
-        self.normals.push(Vector::new(coords.0, coords.1, coords.2).direction());
-        Ok(())
-    }
-
-    fn parse_face(&mut self, s: &str) -> Result<(), Box<Error>> {
-        fn read_group(s: &str) -> Result<(usize, usize, usize), Box<Error>> {
-            let inds = try!(s.split('/')
-                            .map(|i| i.parse::<usize>().map(|x| x - 1))
-                            .collect::<Result<Vec<_>, _>>());
-            if inds.len() != 3 {
-                return Err(Box::new(ParseObjError));
-            }
-            Ok((inds[0], inds[1], inds[2]))
-        }
-
-        let verts = try!(s.split_whitespace()
-                         .skip(1)
-                         .map(read_group)
-                         .collect::<Result<Vec<_>, _>>());
-
-        if verts.len() != 3 {
-            return Err(Box::new(ParseObjError));
-        }
-        let f = Triangle::with_normals(
-            self.points[verts[0].0], self.points[verts[1].0], self.points[verts[2].0],
-            [self.normals[verts[0].2], self.normals[verts[1].2], self.normals[verts[2].2]]);
-
-        self.faces.push(f);
-        Ok(())
-    }
-
-    fn parse_coordinates(s: &str) -> Result<(f64, f64, f64), Box<Error>> {
-        let coords = try!(s.split_whitespace()
-                          .skip(1)
-                          .map(|s| s.parse::<f64>())
-                          .collect::<Result<Vec<_>, _>>());
-        if coords.len() != 3 {
-            return Err(Box::new(ParseObjError));
-        }
-
-        Ok((coords[0], coords[1], coords[2]))
     }
 }
